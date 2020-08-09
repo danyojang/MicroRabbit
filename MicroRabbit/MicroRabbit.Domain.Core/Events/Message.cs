edf96e4429102b38d056dac76fe36df7df32c458
Daniel Jang <danyojang@gmail.com>
2020-08-09 16:31:39 -0700

    Implemented interfaces and classes for Domain.Core

---
+++ MicroRabbit/MicroRabbit.Domain.Core/Events/Message.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace MicroRabbit.Domain.Core.Events
+{
+    public abstract class Message : IRequest<bool>
+    {
+        public string MessageType { get; protected set; }
+        
+        protected Message()
+        {
+            MessageType = GetType().Name;
+        }
+    }
+}
