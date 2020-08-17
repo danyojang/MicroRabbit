1a7c6e1b36330c1db944acb1d41c20d6980e6922
Daniel Jang <dajang@microsoft.com>
2020-08-16 19:37:39 -0700

    Add repository and dbcontext and account domain model

---
+++ MicroRabbit/MicroRabbit.Banking.Domain/Interfaces/IAccountRepository.cs
@@ -0,0 +1,13 @@
+﻿using MicroRabbit.Banking.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroRabbit.Banking.Domain.Interfaces
+{
+    public interface IAccountRepository
+    {
+        IEnumerable<Account> GetAccounts();
+        
+    }
+}
